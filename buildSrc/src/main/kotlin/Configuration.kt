2072f074bef2afcb928a6e6fde213d2675b24204
Eddy Mendoza <extemendoza@belcorp.biz>
2024-09-23 15:21:39 -0500

    se sube la version de app

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -5,7 +5,7 @@ object Configuration {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 26
     const val TARGET_SDK = 34
-    const val VERSION_CODE = 4
+    const val VERSION_CODE = 5
     const val VERSION_NAME = "1.0.3"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 
